a409c658a48ff49900f659d5ba38b74d614051d6
mfiaschi5 <85950804+mfiaschi5@users.noreply.github.com>
2024-12-20 02:04:29 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
         navbarMenu.classList.toggle("show");
     });
 
-    // Optional: Close the navbar if the user clicks outside of it
+    
     document.addEventListener("click", function(event) {
         if (!toggleButton.contains(event.target) && !navbarMenu.contains(event.target)) {
             navbarMenu.classList.remove("show");
@@ -15,16 +15,16 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 document.getElementById('form').addEventListener('click', function(event) {
-    event.preventDefault(); // Impedisce il reindirizzamento immediato
+    event.preventDefault(); 
 
-    // Invia l'evento a Google Analytics
+    
     gtag('event', 'click', {
         'event_category': 'Link',
         'event_label': 'Form Link',
         'value': 1
     });
 
-    // Reindirizza dopo un breve ritardo
+   
     setTimeout(function() {
         window.location.href = "https://forms.gle/UFXaEexD5GhEXcG8A"; 
     }, 300); 
